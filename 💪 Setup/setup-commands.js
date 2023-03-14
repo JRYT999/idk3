77fba79bb61abca593b9723e6015464bb4d11b2a
JRYT999 <122193357+JRYT999@users.noreply.github.com>
2023-03-14 17:44:10 -0400

    Add files via upload

---
+++ 💪 Setup/setup-commands.js
@@ -0,0 +1,197 @@
+var {
+  MessageEmbed
+} = require(`discord.js`);
+var Discord = require(`discord.js`);
+var config = require(`${process.cwd()}/botconfig/config.json`);
+var ee = require(`${process.cwd()}/botconfig/embed.json`);
+var emoji = require(`${process.cwd()}/botconfig/emojis.json`);
+var {
+  databasing
+} = require(`${process.cwd()}/handlers/functions`);
+const { MessageButton, MessageActionRow, MessageSelectMenu } = require('discord.js')
+module.exports = {
+  name: "setup-commands",
+  category: "💪 Setup",
+  aliases: ["setupcommands", "setup-command", "setupcommand"],
+  cooldown: 5,
+  usage: "setup-commands  -->  Follow the Steps",
+  description: "Enable/Disable specific Commands",
+  memberpermissions: ["ADMINISTRATOR"],
+  type: "info",
+  run: async (client, message, args, cmduser, text, prefix) => {
+    
+    let es = client.settings.get(message.guild.id, "embed");let ls = client.settings.get(message.guild.id, "language")
+    try {
+      function getMenuOptions() {
+        return [
+          {
+            label: "ECONOMY",
+            value: "ECONOMY",
+            emoji: "💸",
+            description: `${client.settings.get(message.guild.id, "ECONOMY") ? "❌ Disable ECONOMY Commands" : "✅ Enable ECONOMY Commands"}`
+          },
+          {
+            label: "SCHOOL",
+            value: "SCHOOL",
+            emoji: "🏫",
+            description: `${client.settings.get(message.guild.id, "SCHOOL") ? "❌ Disable SCHOOL Commands" : "✅ Enable SCHOOL Commands"}`
+          },
+          {
+            label: "MUSIC",
+            value: "MUSIC",
+            emoji: "🎶",
+            description: `${client.settings.get(message.guild.id, "MUSIC") ? "❌ Disable Music Commands" : "✅ Enable Music Commands"}`
+          },
+          {
+            label: "FILTER",
+            value: "FILTER",
+            emoji: "👀",
+            description: `${client.settings.get(message.guild.id, "FILTER") ? "❌ Disable FILTER Commands" : "✅ Enable FILTER Commands"}`
+          },
+          {
+            label: "CUSTOMQUEUE",
+            value: "CUSTOMQUEUE",
+            emoji: "⚜️",
+            description: `${client.settings.get(message.guild.id, "CUSTOMQUEUE") ? "❌ Disable CUSTOM-QUEUE Commands" : "✅ Enable CUSTOM-QUEUE Commands"}`
+          },
+          {
+            label: "PROGRAMMING",
+            value: "PROGRAMMING",
+            emoji: "⌨️",
+            description: `${client.settings.get(message.guild.id, "PROGRAMMING") ? "❌ Disable PROGRAMMING Commands" : "✅ Enable PROGRAMMING Commands"}`
+          },
+          {
+            label: "RANKING",
+            value: "RANKING",
+            emoji: "📈",
+            description: `${client.settings.get(message.guild.id, "RANKING") ? "❌ Disable RANKING Commands" : "✅ Enable RANKING Commands"}`
+          },
+          {
+            label: "SOUNDBOARD",
+            value: "SOUNDBOARD",
+            emoji: "🔊",
+            description: `${client.settings.get(message.guild.id, "SOUNDBOARD") ? "❌ Disable SOUNDBOARD Commands" : "✅ Enable SOUNDBOARD Commands"}`
+          },
+          {
+            label: "VOICE",
+            value: "VOICE",
+            emoji: "🎤",
+            description: `${client.settings.get(message.guild.id, "VOICE") ? "❌ Disable VOICE Commands" : "✅ Enable VOICE Commands"}`
+          },
+          {
+            label: "FUN",
+            value: "FUN",
+            emoji: "🕹️",
+            description: `${client.settings.get(message.guild.id, "FUN") ? "❌ Disable FUN Commands" : "✅ Enable FUN Commands"}`
+          },
+          {
+            label: "MINIGAMES",
+            value: "MINIGAMES",
+            emoji: "🎮",
+            description: `${client.settings.get(message.guild.id, "MINIGAMES") ? "❌ Disable MINIGAMES Commands" : "✅ Enable MINIGAMES Commands"}`
+          },
+          {
+            label: "ANIME",
+            value: "ANIME",
+            emoji: "😳",
+            description: `${client.settings.get(message.guild.id, "ANIME") ? "❌ Disable ANIME Commands" : "✅ Enable ANIME Commands"}`
+          },
+          {
+            label: "NSFW",
+            value: "NSFW",
+            emoji: "🔞",
+            description: `${client.settings.get(message.guild.id, "NSFW") ? "❌ Disable NSFW Commands" : "✅ Enable NSFW Commands"}`
+          },
+        ];
+      }
+      function getMenuRowComponent() { 
+        let menuOptions = getMenuOptions();
+        let menuSelection = new MessageSelectMenu()
+          .setCustomId("MenuSelection")
+          .setPlaceholder("Click: enable/disable Command-Categories")
+          .setMinValues(1)
+          .setMaxValues(menuOptions.length)
+          .addOptions(menuOptions.filter(Boolean))
+        return [new MessageActionRow().addComponents(menuSelection)]
+      }
+
+
+      let embed = new Discord.MessageEmbed()
+        .setTitle(`Setup the allowed/not-allowed Command-Categories of this Server`)
+        .setColor(es.color)
+        .setDescription(`**In the selection down below all Categories are listed**\n\n**Select it to either disable/enable it!**\n\n**You can select all (*at least 1*) Command-Categories if you want to disable/enable all of them at once!**`)
+
+       //Send message with buttons
+      let msg = await message.reply({   
+        embeds: [embed], 
+        components: getMenuRowComponent()
+      });
+      const collector = msg.createMessageComponentCollector({filter: (i) => i?.isSelectMenu() && i?.user && i?.message.author.id == client.user.id, time: 180e3, max: 1 });
+      collector.on("collect", async b => {
+        if(b?.user.id !== message.author.id)
+        return b?.reply({content: ":x: Only the one who typed the Command is allowed to select Things!", ephemeral: true});
+     
+        let enabled = 0, disabled = 0;
+        for(const value of b?.values) {
+          let oldstate = client.settings.get(message.guild.id, `${value.toUpperCase()}`);
+          if(!oldstate) enabled++;
+          else disabled++;
+          client.settings.set(message.guild.id, !oldstate, `${value.toUpperCase()}`)
+        }
+        b?.reply(`<a:yes:833101995723194437>  **\`Enabled ${enabled} Command-Categories\` and \`Disabled ${disabled} Command-Categories\` out of \`${b?.values.length} selected Command-Categories\`**`)
+      })
+      collector.on('end', collected => {
+        msg.edit({content: ":x: Time ran out/Input finished! Cancelled", embeds: [
+          msg.embeds[0]
+            .setDescription(`${getMenuOptions().map(option => `> ${option.emoji} **${option.value}-Commands**: ${option.description.split(" ")[0] != "❌" ? `\`Are now disabled [❌]\`` : `\`Are now enabled [✅]\``}`).join("\n\n")}`)
+        ], components: []}).catch((e)=>{})
+      });
+    } catch (e) {
+      console.log(String(e.stack).grey.bgRed)
+      return message.reply({embeds: [new MessageEmbed()
+        .setColor(es.wrongcolor).setFooter(client.getFooter(es))
+        .setTitle(client.la[ls].common.erroroccur)
+        .setDescription(eval(client.la[ls]["cmds"]["setup"]["setup-commands"]["variable5"]))
+      ]});
+    }
+  },
+};
+/**
+ * @INFO
+ * Bot Coded by Tomato#6966 | https://discord.gg/milrato
+ * @INFO
+ * Work for Milrato Development | https://milrato.eu
+ * @INFO
+ * Please mention him / Milrato Development, when using this Code!
+ * @INFO
+ */
+function getNumberEmojis() {
+  return [
+    "<:Number_0:1048612335306154054>",
+    "<:Number_1:1048609592822738954>",
+    "<:Number_2:1048612338187644971>",
+    "<:Number_3:1048609596119466015>",
+    "<:Number_4:1048612340691632139>",
+    "<:Number_5:1048609599223238776>",
+     "<:Number_6:1048612343455698984>",
+     "<:Number_7:1048612345485725747>",
+     "<:Number_8:1048609602754846750>",
+     "<:Number_9:1048612347020853299>",
+     "<:Number_10:1048612349436764200>",
+     "<:Number_11:1048609606630379620>",
+     "<:Number_12:1048612352003686400>",
+     "<:Number_13:1048612353559769098>",
+     "<:Number_14:1048609609788686416>",
+     "<:Number_15:1048612356567093288>",
+     "<:Number_16:1048640855793213590>",
+     "<:Number_17:1048609613089620039>",
+     "<:Number_18:1048612360396492900>",
+     "<:Number_19:1048612362938232903>",
+     "<:Number_20:1048609617808216104>",
+     "<:Number_21:1048640859459043348>",
+     "<:Number_22:1048609621193019453>",
+     "<:Number_23:1048609623583768726>",
+     "<:Number_24:1048640861547810957>",
+     "<:Number_25:1048609627593506816>"
+  ]
+}
